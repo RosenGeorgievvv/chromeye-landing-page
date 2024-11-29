86f172422f73c467c6cf73605b04fcf62a28e381
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-11-29 11:03:20 +0200

    Project update
    Saving my current work on responsiveness.

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -1,25 +1,48 @@
-import React from 'react'
-import logo from '../../assets/logo/chromeye_logo_small_v1.svg';
-import "./navbar.scss"
+import React, { useState } from "react";
+import logo from "../../assets/logo/chromeye_logo_small_v1.svg";
+import hamburger from "../../assets/icons/hamburger.png";
+import "./navbar.scss";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <header>
-        <div className='logo'>
-            <img src={logo} alt='Chromeye Logo' className='logo-img' />
-        </div>
-        <nav className='links'>
-            <ul>
-                <li>Home</li>
-                <li>Services</li>
-                <li>Blog</li>
-                <li>About</li>
-                <li>Agency</li>
-            </ul>
-        </nav>
-        <button className='touch'>Get in Touch</button>
+      <div className="logo">
+        <img src={logo} alt="Chromeye Logo" className="logo-img" />
+      </div>
+      <nav className={`links ${menuOpen ? "hidden" : ""}`}>
+        <ul>
+          <li>Home</li>
+          <li>Services</li>
+          <li>Blog</li>
+          <li>About</li>
+          <li>Agency</li>
+        </ul>
+      </nav>
+      <button className="touch">Get in Touch</button>
+      <button
+        className={`hamburger ${menuOpen ? "active" : ""}`}
+        onClick={toggleMenu}
+      >
+        <img src={hamburger} alt="Hamburger Menu" />
+      </button>
+      <div className={`dropdown ${menuOpen ? "open" : ""}`}>
+        <ul>
+          <li>Home</li>
+          <li>Services</li>
+          <li>Blog</li>
+          <li>About</li>
+          <li>Agency</li>
+        </ul>
+        <button className="touch">Get in Touch</button>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
