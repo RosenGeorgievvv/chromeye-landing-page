a4b52c85e0b7d19d25a5afdb0f7bdb06d0f738a8
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-11-29 13:57:06 +0200

    Update

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,12 +5,14 @@ import './banner.scss';
 const Banner = () => {
   return (
     <div className="banner">
-      <div className="banner-content">
-        <h1 className="banner-title">Ready to Get Started</h1>
-        <p className="banner-description">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique sed scelerisque arcu scelerisque ultrices. Habitant ac semper lorem.
-        </p>
-        <button className="banner-button">Request a Call</button>
+      <div className="wrapper">
+        <div className="banner-content">
+          <h1 className="banner-title">Ready to Get Started</h1>
+          <p className="banner-description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique sed scelerisque arcu scelerisque ultrices. Habitant ac semper lorem.
+          </p>
+          <button className="banner-button">Request a Call</button>
+        </div>
       </div>
     </div>
   );
