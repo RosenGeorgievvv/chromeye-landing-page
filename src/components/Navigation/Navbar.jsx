0c338334f326b798de9685dfdd8a2b7d91139faa
Rosen Georgiev <rosengeorgiew1@gmail.com>
2024-12-10 17:22:18 +0200

    Update Navbar.jsx

--- src/components/Navigation/Navbar.jsx
+++ src/components/Navigation/Navbar.jsx
@@ -1,15 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import logo from "../../assets/logo/chromeye_logo_small_v1.svg";
 import hamburger from "../../assets/icons/hamburger.png";
 import close from "../../assets/icons/close.png";
+import { BASE_URL } from "../../constants/api";
 import "./navbar.scss";
 
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
+  const [links, setLinks] = useState([]);
+
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen);
   };
+
+  // Fetch navigation links dynamically
+  useEffect(() => {
+    const fetchLinks = async () => {
+      try{
+        const response = await fetch(BASE_URL);
+        if(!response.ok){
+          throw new Error(`Failed to fetch navigation links! Status: ${response.status}`);
+        }
+        const data = await response.json();
+        const orderLinks = (data.main_menu || []).sort((a, b) => a.order - b.order).filter(item => item.url);
+        setLinks(orderLinks);
+      } catch(error) {
+        console.error("Failed to fetch main menu:", error);
+      }
+    }
+    fetchLinks();
+  }, [])
 
   return (
     <header className="navbar">
@@ -19,14 +40,13 @@ const Navbar = () => {
         </div>
         <nav className={`links ${menuOpen ? "hidden" : ""}`}>
           <ul>
-            <li>Home</li>
-            <li>Services</li>
-            <li>Blog</li>
-            <li>About</li>
-            <li>Agency</li>
+            {links.map((link, index) => (
+              <li key={index}>
+                {link.url ? <a href={link.url}>{link.name}</a> : link.name}
+              </li>
+            ))}
           </ul>
         </nav>
-        <button className="touch">Get in Touch</button>
         <button
           className={`hamburger ${menuOpen ? "active" : ""}`}
           onClick={toggleMenu}
@@ -38,17 +58,17 @@ const Navbar = () => {
         </button>
         <div className={`dropdown ${menuOpen ? "open" : ""}`}>
           <ul>
-            <li>Home</li>
-            <li>Services</li>
-            <li>Blog</li>
-            <li>About</li>
-            <li>Agency</li>
+            {links.map((link, index) => (
+              <li key={index}>
+                {link.url ? <a href={link.url}>{link.name}</a> : link.name}
+              </li>
+            ))}
           </ul>
-          <button className="touch-dropdown">Get in Touch</button>
         </div>
       </div>
     </header>
   );
 };
+
 
 export default Navbar;
